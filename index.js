4684d827e71ac72f2f8a37086912bd34d7ccec49
LeNam24 <102129617+LeNam24@users.noreply.github.com>
2023-11-06 21:18:08 +0700

    Update new

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Welcome express service!!!')
 })
 
+app.get('/api', () => {
+  res.send('API Documentation')
+})
+
 app.listen(3000, () => {
   console.log('App listening on port: 3000')
 })
